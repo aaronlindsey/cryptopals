3a38fd30b1b566e16425f6fb08181c02080dff04
Aaron Lindsey <1781978+aaronlindsey@users.noreply.github.com>
2022-06-10 19:39:55 -0700

    Solve 04

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Aaron solves https://cryptopals.com/
+# cryptopals
+
+Aaron solves <https://cryptopals.com/>.
+
+To run all tests:
+
+```shell
+python3 -m unittest discover -p 'challenge*.py'
+```
--- challenge03.py
+++ challenge03.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python3
 
-import base64
+import string
 import unittest
 from challenge02 import fixed_XOR
 
-def decrypt(ciphertext: bytes) -> bytes:
-    keys = range(ord('A'), ord('Z'))
+
+def crack_single_char_XOR(ciphertext: bytes) -> bytes:
     max_score = -1
+    keys = [bytes([ord(c)] * len(ciphertext)) for c in string.printable]
+
     for key in keys:
-        padded_key = bytes([key] * len(ciphertext))
-        plaintext = fixed_XOR(ciphertext, padded_key)
-        plaintext_score = score(plaintext)
+        plaintext = fixed_XOR(ciphertext, key)
+        plaintext_score = score_single_char_XOR(plaintext)
         if max_score < plaintext_score:
             max_score = plaintext_score
             result = plaintext
+
     return result
 
-def score(plaintext: bytes) -> bytes:
-    return sum(chr(c).isalpha() for c in plaintext)
+
+PRINTABLE_BYTES = set(bytes(string.printable, 'ascii'))
+
+
+def score_single_char_XOR(plaintext: bytes) -> bytes:
+    score = 0
+
+    for b in plaintext:
+        if b not in PRINTABLE_BYTES:
+            return 0
+
+        c = chr(b)
+        if c.islower():
+            score += 4
+        elif c.isupper():
+            score += 2
+        elif c.isnumeric() or c.isspace():
+            score += 1
+
+    return score
+
 
 class TestChallenge03(unittest.TestCase):
     def test(self):
         ciphertext = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
-        plaintext = decrypt(ciphertext)
+        plaintext = crack_single_char_XOR(ciphertext)
         self.assertEqual(plaintext, b"Cooking MC's like a pound of bacon")
-        pass
+
 
 if __name__ == '__main__':
     unittest.main()
